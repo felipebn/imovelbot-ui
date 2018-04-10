35430bc8c379190a463be48470f599c14c88eeae
felipebn <thrash.nation@gmail.com>
2018-04-10 22:43:47 +0100

    PLaceholder files for filter area

---
+++ src/components/FilterArea.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './FilterArea.css';
+
+class FilterArea extends Component {
+  render(){
+      return (
+        <div className="">
+
+        </div>
+      )
+  }
+}
+
+export default FilterArea;
